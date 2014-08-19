8b24a1905c11922f76897953ecded8b680935c90
Slava Yozhkin <the.yoprst@ya.ru>
2014-08-19 21:17:22 +0400

    Output desc of params, improve params design.

--- popup/properties/base.js
+++ popup/properties/base.js
@@ -12,9 +12,16 @@ define(function() {
   TeamcityProperty.prototype = {
     populateRow: function(row) {
       var cell0 = row.insertCell(0);
+      cell0.className = 'property-name';
       var cell1 = row.insertCell(1);
       cell0.innerHTML = this.label || this.name;
       this.createInput(cell1);
+      if (this.description) {
+        var desc = document.createElement('div');
+        desc.innerHTML = this.description;
+        desc.className = 'property-description';
+        cell1.appendChild(desc);
+      }
     }
   };
   return TeamcityProperty;
